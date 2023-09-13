20fc02d4953844ec1dacded95138d19fe043b713
Zeallotx <134969125+Zeallotx@users.noreply.github.com>
2023-09-13 16:50:30 -0600

    Termino del primer Hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-
+import { useState } from 'react';
 import './App.css';
 import Header from './componentes/Header/Header';
 import Formulario from './componentes/Formulario/Formulario';
+import MiOrg from './componentes/MiOrg';
 
 function App() {
+  const [mostrarFormulario, actualizarMostrar] = useState(true)
+
+  //Ternario --> condition ? seMuestra : noSeMuestra
+  // condition && seMuestra
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario)
+  }
+
   return (
     <div>
       <Header/>
-      <Formulario/>
+      {mostrarFormulario === true ? <Formulario/> : <></>}
+      <MiOrg cambiarMostrar={cambiarMostrar}/>
     </div>
   );
 }
--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,10 +2,12 @@ import "./CampoTexto.css"
 
 const CampoTexto = (props) => {
     const placeholderModificador = `${props.placeholder}...`
-    return <div className="campo-texto">
+    return (
+      <div className="campo-texto">
         <label>{props.titulo}</label>
-        <input placeholder={placeholderModificador}/>
-    </div>
+        <input placeholder={placeholderModificador} required={props.required} />
+      </div>
+    );
 }
 
 export default CampoTexto
